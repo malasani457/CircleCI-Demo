4dd21538684772bbeac3bbbea3d015003c2f416f
venkatesh457 <malasanivenkatesh045@gmail.com>
2019-06-04 15:54:15 +0530

    Changes in shall acript

--- build/packageCreate-Install.sh
+++ build/packageCreate-Install.sh
@@ -3,7 +3,7 @@ Package_Version=""
 TARGET_ORG=
 
 echo "Creating new package version for BrillioTest"
-  Package_Version="$(sfdx force:package:version:create -p BrillioTest -d force-app -w 10 -v --targetdevhubusername=dev0@brillio.com)"
+  Package_Version="$(sfdx force:package:version:create -p BrillioTest -d force-app -w 10 -v --targetdevhubusername dev0@brillio.com)"
   sleep 300 # We've to wait for package replication.
 
 echo "Package installation es-base-objects"
