70bf10bde6f7befe147c6788346ff7c8f98cce60
venkatesh457 <malasanivenkatesh045@gmail.com>
2019-06-10 10:52:31 +0530

    Changes in Package shall script

--- build/packageCreate-Install.sh
+++ build/packageCreate-Install.sh
@@ -1,12 +1,13 @@
-echo "Package installation process started..."
+echo "Unlocked Package process started..."
 Package_Version=""
 TARGET_ORG=
 
 echo "Creating new package version for BrillioTest"
   Package_Version="$(sfdx force:package:version:create -p BrillioTest -d force-app -k test1234 -w 10 -v DEV)"
   sleep 300 # We've to wait for package replication.
+echo "Package version is Done!"
 
-echo "Package installation es-base-objects.gt"
-$SFDX_CLI_EXEC force:package:install --package $Package_Version -w 10 -r -u UAT
+echo "Package installation process started"
+sfdx force:package:install --package $Package_Version -w 10 -r -u UAT
 
-echo "...Done!"
+echo "Installed Sucessfully...Done!"
