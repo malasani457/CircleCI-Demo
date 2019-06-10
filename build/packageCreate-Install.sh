42f21639830ee1d2b9a43557240fce2237ad373c
venkatesh457 <malasanivenkatesh045@gmail.com>
2019-06-10 09:58:32 +0530

    Commit to test

--- build/packageCreate-Install.sh
+++ build/packageCreate-Install.sh
@@ -6,7 +6,7 @@ echo "Creating new package version for BrillioTest"
   Package_Version="$(sfdx force:package:version:create -p BrillioTest -d force-app -w 10 -v --targetdevhubusername dev0@brillio.com)"
   sleep 300 # We've to wait for package replication.
 
-echo "Package installation es-base-objects"
+echo "Package installation es-base-objects.gt"
 $SFDX_CLI_EXEC force:package:install --package $Package_Version -w 10 -r -u UAT
 
 echo "...Done!"
