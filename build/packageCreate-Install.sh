ac059be94d67cc3f81d91b1ad604c15fadf71c8e
venkatesh457 <malasanivenkatesh045@gmail.com>
2019-06-10 11:23:56 +0530

    variable printing for version control

--- build/packageCreate-Install.sh
+++ build/packageCreate-Install.sh
@@ -6,7 +6,9 @@ echo "Creating new package version for BrillioTest"
   Package_Version="$(sfdx force:package:version:create -p BrillioTest -d force-app -k test1234 -w 10 -v DEV)"
   sleep 300 # We've to wait for package replication.
 echo "Package version is Done!"
-echo  "$Package_Version"
+echo "******************************"
+echo  $Package_Version
+echo "******************************"
 
 echo "Package installation process started"
 sfdx force:package:install --package $Package_Version -w 10 -r -u UAT
